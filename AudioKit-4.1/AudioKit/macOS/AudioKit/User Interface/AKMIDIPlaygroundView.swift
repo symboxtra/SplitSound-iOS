7c8a4b6aae87fcb917a5b602feeb0172862e1796
Jack McKernan <jmcker@outlook.com>
2018-03-21 09:24:25 -0400

    Update travis and XCode to use CocoaPods

--- AudioKit-4.1/AudioKit/macOS/AudioKit/User Interface/AKMIDIPlaygroundView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AKMIDIKeyboardPlaygroundView.swift
-//  AudioKit For macOS
-//
-//  Created by Aurelius Prochazka on 6/22/17.
-//  Copyright © 2017 AudioKit. All rights reserved.
-//
-
-import Foundation
--- Pods/Target Support Files/Pods-SplitSoundUITests/Pods-SplitSoundUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SplitSoundUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SplitSoundUITests
-@end
