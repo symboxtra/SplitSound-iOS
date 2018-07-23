36f70eebeceb05777cbc6741f36966f0a9686688
Davis Jordan <davisridleyjordan@gmail.com>
2018-07-23 00:42:36 -0500

    Button Tests

--- SplitSoundUITests/SplitSoundUITests.swift
+++ SplitSoundUITests/SplitSoundUITests.swift
@@ -7,18 +7,16 @@
 //
 
 import XCTest
-
+@testable import SplitSound
 class SplitSoundUITests: XCTestCase {
-        
+    
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        //setup testing environment
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
+        //app.launchArguments.append("--uitesting")
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -28,9 +26,13 @@ class SplitSoundUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testMainScreen() {
+        //determine existence of buttons
+        XCTAssertTrue(XCUIApplication().buttons["BBB"].exists)
+        XCTAssertTrue(XCUIApplication().buttons["Play"].exists)
+        XCTAssertTrue(XCUIApplication().buttons["SkipBack"].exists)
+        XCTAssertTrue(XCUIApplication().buttons["SkipForward"].exists)
+        
     }
     
 }
