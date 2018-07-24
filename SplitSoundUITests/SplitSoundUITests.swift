e2670aa88e585c5eb06b701e172fc0a0a7052765
Davis Jordan <davisridleyjordan@gmail.com>
2018-07-24 13:02:23 -0500

    Listener Table Tests

--- SplitSoundUITests/SplitSoundUITests.swift
+++ SplitSoundUITests/SplitSoundUITests.swift
@@ -33,4 +33,27 @@ class SplitSoundUITests: XCTestCase {
         app.navigationBars["SplitSound.TableView"].buttons["Home"].tap()
     }
     
+    //test the listener table
+    func testListenerTable() {
+        
+        let app = XCUIApplication()
+        app.toolbars["Bottom Bar"].buttons["Other Listeners"].tap()
+        
+        let tablesQuery = app.tables
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["I can't think of another one"]/*[[".cells.staticTexts[\"I can't think of another one\"]",".staticTexts[\"I can't think of another one\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let theBoiRegoStaticText = tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["The boi Rego"]/*[[".cells.staticTexts[\"The boi Rego\"]",".staticTexts[\"The boi Rego\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        theBoiRegoStaticText.tap()
+        
+        let designChoiceStaticText = tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Design choice"]/*[[".cells.staticTexts[\"Design choice\"]",".staticTexts[\"Design choice\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        designChoiceStaticText.tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Georgey Boi"]/*[[".cells.staticTexts[\"Georgey Boi\"]",".staticTexts[\"Georgey Boi\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Gustavo"]/*[[".cells.staticTexts[\"Gustavo\"]",".staticTexts[\"Gustavo\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        theBoiRegoStaticText.tap()
+        designChoiceStaticText.tap()
+        app.navigationBars["SplitSound.TableView"].buttons["Home"].tap()
+        
+        
+    }
+    
 }
