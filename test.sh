5384d9d65790a9a9c105c42547786781ee2ac104
Jack McKernan <jmcker@outlook.com>
2018-07-24 20:44:02 -0500

    Try removing xcpretty

--- test.sh
+++ test.sh
@@ -7,5 +7,5 @@ source scripts/env-test.sh
 echo -------- Testing... --------
 echo
 
-xcodebuild test -enableCodeCoverage YES -workspace SplitSound.xcworkspace -scheme SplitSound -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 8,OS=latest' "$CFLAGS" | xcpretty --color
+xcodebuild test -enableCodeCoverage YES -workspace SplitSound.xcworkspace -scheme SplitSound -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 8,OS=latest' "$CFLAGS"
 
